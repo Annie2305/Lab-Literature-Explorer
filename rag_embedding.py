6ee337f97a7a7a8fe842a45ae8cd20a89967748e
Annie_Teng <165318564+Annie2305@users.noreply.github.com>
2025-06-07 01:19:35 +0800

    Add files via upload

--- rag_embedding.py
+++ rag_embedding.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
